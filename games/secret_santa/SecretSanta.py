5dded33849670671172e459a0355fc7e2764de15
Jake Lipson <jake_lipson@yahoo.com>
2019-12-13 19:19:13 -0500

    reformatted project, added CSV import

--- games/secret_santa/SecretSanta.py
+++ games/secret_santa/SecretSanta.py
@@ -1,60 +1,50 @@
-# TODO: Update readme to reflect new menu
-# TODO: Create CSV import logic
-
 import random
 import csv
+from helpers.CSVHelpers import csv_import
 
 
-def make_matches(people):
-    for person in people:
-        match = random.randint(0, 5)
-        while (people[match] == person) or (people[match][3] == True):
+class SecretSanta(object):
+
+    def make_matches(self, people):
+        for person in people:
             match = random.randint(0, 5)
-        person[2] = match
-        people[match][3] = True
-    return people
+            while (people[match] == person) or (people[match][3] == True):
+                match = random.randint(0, 5)
+            person[2] = match
+            people[match][3] = True
+        return people
 
 
-def print_matches(people):
-    for person in people:
-        print(f"\n{person[0]} is {people[person[2]][0]}'s Santa!")
+    def print_matches(self, people):
+        for person in people:
+            print(f"\n{person[0]} is {people[person[2]][0]}'s Santa!")
+        print("\n")
 
 
-def main_code_import():
-    # [name, email, match, chosen]
-    people = [
-        ["Jake", "jake_lipson@yahoo.com", -1, False],
-        ["Lukas", "", -1, False],
-        ["Jason", "", -1, False],
-        ["Chris", "", -1, False],
-        ["Besnik", "", -1, False],
-        ["Mike", "", -1, False],
-    ]
+    def main_code_import(self):
+        # [name, email, match, chosen]
+        people = [
+            ["Person", "email@email.com", -1, False],
+        ]
 
-    people = make_matches(people)
-    print_matches(people)
+        people = self.make_matches(people)
+        self.print_matches(people)
 
 
-def main_csv_import():
-    # people = []
-    with open('/config/people.csv', 'rt') as f:
-        data = csv.reader(f)
-        for row in data:
-            print(row)
+    def main_csv_import(self):
+        people = csv_import("config/people.csv")
+        self.print_matches(people)
 
 
-def start():
-    # Main menu
-    import_type = input("\nWould you like to:\n\n1. import names from a CSV?\n2. Import names from code\n\n(Please type the number corresponding to your answer and then press enter)\n")
-    while (import_type != '1') and (import_type != '2'):
-        print("That response was invalid. Please choose from the options")
-        import_type = input("\nWould you like to:\n\n1. import names from a CSV?\n2. Import names from code\n\n(Please type the number corresponding to your answer and then press enter)\n")
-    # Menu logic
-    import_type = int(import_type)
-    if (import_type == 1):
-        main_csv_import()
-    elif (import_type == 2):
-        main_code_import()
-
-if __name__ == "__main__":
-    start()
+    def start(self):
+        # Main menu
+        import_type = input("\nWould you like to:\n\n1. Import names from a CSV\n2. Import names from code\n\n(Please type the number corresponding to your answer and then press enter)\n\n")
+        while (import_type != '1') and (import_type != '2'):
+            print("That response was invalid. Please choose from the options")
+            import_type = input("\nWould you like to:\n\n1. import names from a CSV?\n2. Import names from code\n\n(Please type the number corresponding to your answer and then press enter)\n\n")
+        # Menu logic
+        import_type = int(import_type)
+        if (import_type == 1):
+            self.main_csv_import()
+        elif (import_type == 2):
+            self.main_code_import()
---
+++ main.py
@@ -0,0 +1,23 @@
+# TODO: Update readme to reflect new menu
+# TODO: Setup email to participants
+
+from games.secret_santa.SecretSanta import SecretSanta
+
+
+def main():
+    # Main menu
+    game_type = input("\nWould you like to play:\n1. Secret Santa\n\n")
+    while (game_type != '1'):
+        print("That response was invalid. Please choose from the options")
+        game_type = input("\nWould you like to play:\n1. Secret Santa\n\n")
+    game_type = int(game_type)
+
+    # Menu Logic
+    if (game_type == 1):
+        game = SecretSanta()
+
+    game.start()
+
+
+if __name__ == "__main__":
+    main()
