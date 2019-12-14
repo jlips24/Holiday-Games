aa83822941d28e5802dca1776d9e0a1cab166229
Jake Lipson <jake_lipson@yahoo.com>
2019-12-13 22:55:02 -0500

    SecretSanta complete

--- games/secret_santa/SecretSanta.py
+++ games/secret_santa/SecretSanta.py
@@ -8,9 +8,10 @@ class SecretSanta(object):
 
     def make_matches(self, people):
         for person in people:
-            match = random.randint(0, 5)
+            match = random.randint(0, (len(people) - 1))
+            print(people[match][3])
             while (people[match] == person) or (people[match][3] == True):
-                match = random.randint(0, 5)
+                match = random.randint(0, (len(people) - 1))
             person[2] = match
             people[match][3] = True
         return people
@@ -23,9 +24,9 @@ class SecretSanta(object):
 
 
     def main_csv_import(self):
-        people = csv_import("config/people.csv")
-        self.print_matches(people)
-
+        return csv_import("config/people.csv")
 
     def start(self):
-        self.main_csv_import()
+        people = self.main_csv_import()
+        people = self.make_matches(people)
+        send_emails(people)
--- helpers/EmailHelpers.py
+++ helpers/EmailHelpers.py
@@ -14,28 +14,34 @@ def read_template(filename):
     return Template(template_file_content)
 
 def send_emails(people):
+    print("Sending Messages")
+
     s = smtplib.SMTP(host=os.getenv("EMAIL_HOST"), port=int(os.getenv("EMAIL_PORT")))
     s.starttls()
     s.login(os.getenv("EMAIL_USER"), os.getenv("EMAIL_PASSWORD"))
 
+    message_number = 1
+    total_messages = len(people)
     for person in people:
+        print(f"({round((message_number/total_messages)*100, 2)}%)Sending message to {person[0]} at {person[1]}: message {message_number} of {total_messages}")
         message_template = read_template('templates/SecretSantaEmail.txt')
         
         msg = MIMEMultipart()
-        # add in the actual person name to the message template
+        # Add in the reciprient name and match name to the message template
         reciprient = person[0]
         match_name = people[person[2]][0]
         message = message_template.substitute(RECIPRIENT_NAME=reciprient, MATCH_NAME=match_name)
 
-        # setup the parameters of the message
+        # Setup the parameters of the message
         msg['From'] = os.getenv("EMAIL_USER")
         msg['To'] = person[1]
         msg['Subject']="Your Secret Santa match is ready"
 
-        # add in the message body
+        # Add in the message body
         msg.attach(MIMEText(message, 'plain'))
 
-        # send the message via the server set up earlier.
+        # Aend the message
         s.send_message(msg)
-        
         del msg
+
+        message_number += 1
